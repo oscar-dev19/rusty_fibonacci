3744f0b02030ccb30e9e88ab3b5538322db4c1d4
Oscar Lopez <53553460+oscar-dev19@users.noreply.github.com>
2024-07-30 09:50:13 -0700

    wrote first iteration of fibonacci program.

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::io;
+
+fn main() {
+    let number = input_num_to_calculate();
+    println!("The fibonacci of given number {} is {}", number, calculate_fib(number));
+}
+
+fn input_num_to_calculate() -> i32 {
+    loop {
+        println!("Enter number to calculate fibonacci of:");
+        let mut number = String::new();
+        io::stdin().read_line(&mut number).expect("Failed to read user input!");
+
+        let number: i32 = match number.trim().parse() {
+            Ok(num) => break num,
+            Err(_) => {
+                println!("Please enter a valid number!");
+                continue;
+            },
+        };
+    }
+}
+
+fn calculate_fib(num: i32) -> i32 {
+    let mut a = 0;
+    let mut b = 1;
+    let mut c = 0;
+
+    for _ in 0..num {
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    
+    c
+}
